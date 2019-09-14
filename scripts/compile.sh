fe76eb3cd015d81c7fcd29c4b2fce56401ef445b
Andrés Felipe Torres <dev@felipeandres254.io>
2019-09-14 16:56:28 -0500

    Add CLI messages to scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+HOSTNAME="NodeMCU OTA"
+echo -e "\e[1;34m[$HOSTNAME] \e[0mInitializing..."
 
 # Initialize environment variables
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 if [[ ! -f "$DIR/.env" ]]; then
     cp .env.sample .env
-    echo -e "\e[1;33mWARN \e[0mPlease update your .env file"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;33mWARN \e[0mPlease update your .env file"
     exit 1
 fi
 set -o allexport
@@ -16,19 +18,19 @@ SKETCH="$1"
 UPLOAD="$2"
 
 if [[ -z $SKETCH ]]; then
-    echo -e "\e[1;31mERROR \e[0mMissing sketch name!"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;31mERROR \e[0mMissing sketch name!"
     exit 1
 fi
 if [[ ! -d "$DIR/examples/$SKETCH" ]]; then
-    echo -e "\e[1;31mERROR \e[0mSketch '$SKETCH' not found!"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;31mERROR \e[0mSketch '$SKETCH' not found!"
     exit 1
 fi
 if [[ -z $WIFI_SSID ]]; then
-    echo -e "\e[1;31mERROR \e[0mMissing WiFi SSID!"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;31mERROR \e[0mMissing WiFi SSID!"
     exit 1
 fi
 if [[ -z $WIFI_PASSWORD ]]; then
-    echo -e "\e[1;31mERROR \e[0mMissing WiFi password!"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;31mERROR \e[0mMissing WiFi password!"
     exit 1
 fi
 
@@ -46,7 +48,7 @@ sed -i -E "s/#define WIFI_PSK.*/#define WIFI_PSK \"$WIFI_PASSWORD\"/" "$DIR/.out
 # Initialize Arduino CLI
 shopt -s expand_aliases
 alias arduino-cli='cmd //C "$ARDUINO_PATH/arduino-cli/arduino-cli.exe"'
-arduino-cli config init
+arduino-cli config init >/dev/null 2>&1
 cd "$LOCALAPPDATA"
 echo "proxy_type: auto
 sketchbook_path: $SKETCHBOOK_PATH
@@ -57,18 +59,24 @@ board_manager:
 " > "Arduino15/arduino-cli.yaml"
 
 # Compile sketch
+echo -e "\e[1;34m[$HOSTNAME] \e[0mCompiling sketch..."
 arduino-cli compile --fqbn esp8266:esp8266:nodemcu "$DIR/.output/$name"
 
 # Upload sketch
 cd "$LOCALAPPDATA"
 file=$(find Temp/arduino-sketch-*/$name.ino.bin)
 if [ "$UPLOAD" == "--upload" ]; then
-    curl -F "image=@$file" 192.168.43.228/update
+    echo -e "\e[1;34m[$HOSTNAME] \e[0mUploading binary..."
+    curl -F "image=@$file" 192.168.43.228/update >/dev/null 2>&1
     rm -rf "$DIR/.output/$name"
 else
+    echo -e "\e[1;34m[$HOSTNAME] \e[0mBinary file located in .output/$name.ino.bin"
     mv $file "$DIR/.output"
 fi
 
 # Cleanup
+echo -e "\e[1;34m[$HOSTNAME] \e[0mCleaning..."
 cd "$LOCALAPPDATA"
 rm -rf Arduino15/ Temp/arduino*/
+
+echo -e "\e[1;34m[$HOSTNAME] \e[1;32mOK!\e[0m"
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+HOSTNAME="NodeMCU OTA"
+echo -e "\e[1;34m[$HOSTNAME] \e[0mInitializing..."
 
 # Initialize environment variables
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 if [[ ! -f "$DIR/.env" ]]; then
     cp .env.sample .env
-    echo -e "\e[1;33mWARN \e[0mPlease update your .env file"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;33mWARN \e[0mPlease update your .env file"
     exit 1
 fi
 set -o allexport
@@ -15,15 +17,18 @@ set +o allexport
 BINARY="$1"
 
 if [[ -z $BINARY ]]; then
-    echo -e "\e[1;31mERROR \e[0mMissing binary name!"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;31mERROR \e[0mMissing binary name!"
     exit 1
 fi
 if [[ ! -f "$DIR/.output/nodemcu-${BINARY,,}.ino.bin" ]]; then
-    echo -e "\e[1;31mERROR \e[0mBinary '$BINARY' not found!"
+    echo -e "\e[1;34m[$HOSTNAME] \e[1;31mERROR \e[0mBinary '$BINARY' not found!"
     exit 1
 fi
 
 # Upload sketch
 file="$DIR/.output/nodemcu-${BINARY,,}.ino.bin"
-curl -F "image=@$file" 192.168.43.228/update
+echo -e "\e[1;34m[$HOSTNAME] \e[0mUploading binary..."
+curl -F "image=@$file" 192.168.43.228/update >/dev/null 2>&1
 rm -rf "$file"
+
+echo -e "\e[1;34m[$HOSTNAME] \e[1;32mOK!\e[0m"
