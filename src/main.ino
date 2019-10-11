ed87ead22e8c5bff7e74b9c7153c9a4c6e439b85
Andrés Felipe Torres <dev@felipeandres254.io>
2019-10-11 13:58:57 -0500

    Remove serial outputs

--- src/main.ino
+++ src/main.ino
@@ -19,15 +19,11 @@ ESP8266HTTPUpdateServer httpUpdater;
 void setup() {
     NodeMCU_setup();
 
-    Serial.begin(115200);
-    Serial.println();
-    Serial.println("Booting Sketch...");
     WiFi.mode(WIFI_AP_STA);
     WiFi.begin(ssid, password);
 
     while (WiFi.waitForConnectResult() != WL_CONNECTED) {
         WiFi.begin(ssid, password);
-        Serial.println("WiFi failed, retrying...");
     }
 
     MDNS.begin(host);
@@ -36,7 +32,6 @@ void setup() {
     httpServer.begin();
 
     MDNS.addService("http", "tcp", 80);
-    Serial.printf("NodeMCU ready! Open http://%s.local/update in your browser\n", host);
 }
 
 void loop() {
