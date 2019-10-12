95077d1d7120a9a9af3582dafb60537606342a4e
Andrés Felipe Torres <dev@felipeandres254.io>
2019-10-11 23:18:32 -0500

    Remove mDNS reference

--- src/main.ino
+++ src/main.ino
@@ -1,7 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
 
 #ifndef WIFI_SSID
@@ -9,7 +8,6 @@
 #define WIFI_PSK  "YOUR-PASSWORD"
 #endif
 
-const char* host = "nodemcu";
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PSK;
 
@@ -26,17 +24,12 @@ void setup() {
         WiFi.begin(ssid, password);
     }
 
-    MDNS.begin(host);
-
     httpUpdater.setup(&httpServer);
     httpServer.begin();
-
-    MDNS.addService("http", "tcp", 80);
 }
 
 void loop() {
     httpServer.handleClient();
-    MDNS.update();
 
     NodeMCU_loop();
 }
